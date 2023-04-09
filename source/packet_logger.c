78acdcfd6cd946ef94db5f88c167f70963d4297c
Nick <32660358+dontcrash@users.noreply.github.com>
2023-04-10 00:18:02 +0930

    Update

--- source/packet_logger.c
+++ source/packet_logger.c
@@ -2,15 +2,57 @@
 #include <pthread.h>
 #include "libs/mongoose.h"
 
-int total_packets = 0;
+struct packet {
+    char protocol[10];
+    char source_ip[20];
+    int source_port;
+    char destination_ip[20];
+    int destination_port;
+    int packet_size_bytes;
+};
+
+//TODO This cannot be done this way as it is a hard limit and when full will crash
+struct packet packets[1024000];
+
+int num_packets = 0;
+
+void add_packet(char* protocol, char* source_ip, int source_port, char* destination_ip, int destination_port, int packet_size_bytes) {
+    strcpy(packets[num_packets].protocol, protocol);
+    strcpy(packets[num_packets].source_ip, source_ip);
+    packets[num_packets].source_port = source_port;
+    strcpy(packets[num_packets].destination_ip, destination_ip);
+    packets[num_packets].destination_port = destination_port;
+    packets[num_packets].packet_size_bytes = packet_size_bytes;
+    num_packets++;
+}
+
+int compare_packets_by_sent(const void* a, const void* b) {
+    const struct packet* pa = (const struct packet*)a;
+    const struct packet* pb = (const struct packet*)b;
+    return strcmp(pa->source_ip, pb->source_ip);
+}
 
 static void ev_handler(struct mg_connection *nc, int ev, void *ev_data, void *user_data) {
     if (ev == MG_EV_HTTP_MSG) {
-        //TODO - store packets in memory or in DB, avoid duplicates, combine packets with the same source IP
-        //Keep track of protocols used by each IP etc
-        //Another option is to store the info in a DB such as sqlite?
-        //Need to add URL check and display page requested etc
-        mg_http_reply(nc, 200, "Content-Type: text/plain\r\n", "Total packets: %d", total_packets);
+        qsort(packets, num_packets, sizeof(struct packet), compare_packets_by_sent);
+        int i = num_packets - 1; // start from the end of the array
+        char top_ips[1000] = "";
+        int count = 0;
+        while (i >= 0 && count < 10) { // limit to top 10 IP addresses
+            char current_ip[20];
+            strcpy(current_ip, packets[i].source_ip);
+            int packets_sent = 1;
+            i--;
+            while (i >= 0 && strcmp(current_ip, packets[i].source_ip) == 0) {
+                packets_sent++;
+                i--;
+            }
+            char ip_str[100];
+            sprintf(ip_str, "%s - %d packets<br>", current_ip, packets_sent);
+            strcat(top_ips, ip_str);
+            count++;
+        }
+        mg_http_reply(nc, 200, "Content-Type: text/html\r\n", "<html><body>Top IP addresses by packets sent:<br>%s</body></html>", top_ips);
     }
 }
 
@@ -25,12 +67,16 @@ void *packet_listener_thread(void *dev) {
         exit(1);
     }
 
-    // Read the output line by line
     char output[1024];
-
+    //Loop through output
     while (fgets(output, sizeof(output), fp) != NULL) {
-        //printf("%s", output);
-        ++total_packets;
+        char* protocol = strtok(output, ",");
+        char* source_ip = strtok(NULL, ",");
+        int source_port = atoi(strtok(NULL, ","));
+        char* destination_ip = strtok(NULL, ",");
+        int destination_port = atoi(strtok(NULL, ","));
+        int packet_size_bytes = atoi(strtok(NULL, ",\n"));
+        add_packet(protocol, source_ip, source_port, destination_ip, destination_port, packet_size_bytes);
     }
 
     // Close the process
