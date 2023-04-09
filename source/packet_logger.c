3407c8f3d604e4596fbfbf91944bcef4a0a60c11
Nick <32660358+dontcrash@users.noreply.github.com>
2023-04-10 03:17:31 +0930

    Update packet_logger.c

--- source/packet_logger.c
+++ source/packet_logger.c
@@ -4,6 +4,7 @@
 #include "libs/mongoose.h"
 
 #define MAX_IPS 1000000
+#define TOP_LIST 10
 
 int total_packets = 0;
 
@@ -24,19 +25,50 @@ struct stats {
     char protocols[10][10];
 };
 
-struct stats ip_stats[MAX_IPS];
+struct stats stats[MAX_IPS];
 int num_ips = 0;
 
-//TODO save ip_stats to a file when closing and load it if it exists in the main void
+int cmp_stats_by_bytes_desc(const void* a, const void* b) {
+    const struct stats* sa = (const struct stats*)a;
+    const struct stats* sb = (const struct stats*)b;
+
+    if (sa->total_bytes < sb->total_bytes) {
+        return 1;
+    }
+    else if (sa->total_bytes > sb->total_bytes) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+}
+
+//TODO save stats to a file when closing and load it if it exists in the main void
 
 static void ev_handler(struct mg_connection *nc, int ev, void *ev_data, void *user_data) {
     if (ev == MG_EV_HTTP_MSG) {
         char *header = "Content-Type: text/html\r\n";
-        char unique_text[100];
-        sprintf(unique_text, "IP addresses: %d", num_ips);
-        char packet_text[100];
-        sprintf(packet_text, "Total packets: %d", total_packets);
-        mg_http_reply(nc, 200, header, "<html><body>%s<br>%s</body></html>", unique_text, packet_text);
+
+        // Sort IP stats by total bytes in descending order
+        qsort(stats, num_ips, sizeof(struct stats), cmp_stats_by_bytes_desc);
+
+        //CSS
+        char *css = "<style>td{width:150px;}</style>";
+
+        // Generate HTML to display top 10 IP addresses and total data they have sent
+        char top_ips_html[1024];
+        snprintf(top_ips_html, sizeof(top_ips_html), "<table><tr><td><b>IP Address</b></td><td><b>Data Sent (bytes)</b></td></tr>");
+        for (int i = 0; i < TOP_LIST && i < num_ips; ++i) {
+            snprintf(top_ips_html + strlen(top_ips_html), sizeof(top_ips_html) - strlen(top_ips_html), "<tr><td>%s</td><td>%d</td></tr>", stats[i].ip, stats[i].total_bytes);
+        }
+        snprintf(top_ips_html + strlen(top_ips_html), sizeof(top_ips_html) - strlen(top_ips_html), "</table>");
+
+        // Generate HTML to display total packets and unique IP addresses
+        char stats_html[100];
+        snprintf(stats_html, sizeof(stats_html), "Total packets: %d<br>Unique IP addresses: %d", total_packets, num_ips);
+
+        // Send HTTP response with generated HTML
+        mg_http_reply(nc, 200, header, "<html><head>%s</head><body>%s<br><br>%s</body></html>", css, stats_html, top_ips_html);
     }
 }
 
@@ -65,7 +97,7 @@ void *packet_listener_thread(void *dev) {
         // check if this IP already exists in our stats array
         int ip_index = -1;
         for (int i = 0; i < num_ips; ++i) {
-            if (strcmp(ip_stats[i].ip, source_ip) == 0) {
+            if (strcmp(stats[i].ip, source_ip) == 0) {
                 ip_index = i;
                 break;
             }
@@ -73,21 +105,21 @@ void *packet_listener_thread(void *dev) {
 
         if (ip_index == -1) {
             // new IP, initialize stats
-            strncpy(ip_stats[num_ips].ip, source_ip, sizeof(ip_stats[num_ips].ip) - 1);
-            ip_stats[num_ips].total_packets = 0;
-            ip_stats[num_ips].total_bytes = 0;
-            ip_stats[num_ips].num_protocols = 0;
+            strncpy(stats[num_ips].ip, source_ip, sizeof(stats[num_ips].ip) - 1);
+            stats[num_ips].total_packets = 0;
+            stats[num_ips].total_bytes = 0;
+            stats[num_ips].num_protocols = 0;
             ++num_ips;
         }
 
         // update stats
-        ip_stats[ip_index].total_packets += 1;
-        ip_stats[ip_index].total_bytes += packet_size_bytes;
+        stats[ip_index].total_packets += 1;
+        stats[ip_index].total_bytes += packet_size_bytes;
 
         // check if protocol already exists
         int protocol_index = -1;
-        for (int i = 0; i < ip_stats[ip_index].num_protocols; ++i) {
-            if (strcmp(ip_stats[ip_index].protocols[i], protocol) == 0) {
+        for (int i = 0; i < stats[ip_index].num_protocols; ++i) {
+            if (strcmp(stats[ip_index].protocols[i], protocol) == 0) {
                 protocol_index = i;
                 break;
             }
@@ -95,8 +127,8 @@ void *packet_listener_thread(void *dev) {
 
         if (protocol_index == -1) {
             // new protocol for this IP, add to protocols array
-            strncpy(ip_stats[ip_index].protocols[ip_stats[ip_index].num_protocols], protocol, sizeof(ip_stats[ip_index].protocols[0]) - 1);
-            ++ip_stats[ip_index].num_protocols;
+            strncpy(stats[ip_index].protocols[stats[ip_index].num_protocols], protocol, sizeof(stats[ip_index].protocols[0]) - 1);
+            ++stats[ip_index].num_protocols;
         }
     }
 
